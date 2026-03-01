5f687d204d4ca3bf7335645827f631b101c0fdb0
HelenJenifer <helenjohnantru@gmail.com>
2026-03-01 12:55:31 +0530

    Add files via upload
    Day1

---
+++ day01/classroom.js
@@ -0,0 +1,20 @@
+
+var firstName ="ABC"
+console.log(typeof(firstName))
+
+var companyName ="xyz"
+console.log(typeof(companyName))
+var mobileNumber =46465656565656
+console.log(typeof(mobileNumber))
+var isAutomation =true
+console.log(typeof(isAutomation))
+var hasPlaywright
+console.log(typeof(hasPlaywright))
+
+/*Assignment Requirements: Create the following variables using var and check their typeOf
+a) firstName
+b) companyName
+c) mobileNumber
+d) isAutomation
+e) hasPlaywright (do not assign)
+Print and confirm the values and data types*/
