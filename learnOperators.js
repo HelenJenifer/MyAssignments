952e1eecb5f51776f2e2193e2ce7fb51fb5b05fa
HelenJenifer <helenjohnantru@gmail.com>
2026-03-07 13:21:45 +0530

    Add files via upload
    commit

---
+++ learnOperators.js
@@ -0,0 +1,13 @@
+// arithematic operators
+// +,-,*,%,/
+
+// comparison opertors---> false/true -->boolean
+//>,<,==, >=,<= ,!=
+
+let a =3 ,b= 4
+// lose equality ->==
+//not equality -->!=d
+console.log(a==b)
+//stirct equality-->===
+let x= 3 ,z ='3'
+console.log(x===z)
